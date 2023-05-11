14a3f8d83b411bf60bc0d10c6f0b430a19986a50
Lukas <lukasg.dasilva2014@gmail.com>
2023-05-11 10:38:59 -0300

    melhor config até agora

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-let listVideos = document.querySelectorAll('.video-list .vid')
-let mainVideo = document.querySelector('.main-video video')
-let title = document.querySelector('.main-video .title')
+import videos from "./videos.js"
 
+function loadVideos() {
+    let listVideos = document.querySelector('.video-list')
+    let mainVideo = document.querySelector('.main-video video')
+    let title = document.querySelector('.main-video .title')
 
-listVideos.forEach(video => {
-    video.addEventListener('click', () => {
-        listVideos.forEach(vid => vid.classList.remove('active'))
-        video.classList.add('active')
+    mainVideo.src = videos[0].src
+    title.innerHTML = videos[0].title
 
-        if (video.classList.contains('active')) {
-            //let src = video.children[0].getAttribute('src')
-            mainVideo.src = video.children[0].getAttribute('src')
-            //let text = video.children[1].innerHTML
-            title.innerHTML = video.children[1].innerHTML
-        }
+    videos.forEach((video, index) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <div class="${index + 1 === 1 ? "vid active" : "vid"}">
+            <video src=${video.src} muted></video>
+            <h3 class="title">${video.title}</h3>
+        </div>
+        `
+        listVideos.appendChild(div)
     })
-})
+    clickOnVideo()
+}
+
+function clickOnVideo() {
+    let listVideos = document.querySelectorAll('.video-list .vid')
+    let mainVideo = document.querySelector('.main-video video')
+    let title = document.querySelector('.main-video .title')
+
+    listVideos.forEach(video => {
+        video.addEventListener('click', () => {
+            listVideos.forEach(vid => vid.classList.remove('active'))
+            video.classList.add('active')
+
+            if (video.classList.contains('active')) {
+                mainVideo.src = video.children[0].getAttribute('src')
+                title.innerHTML = video.children[1].innerHTML
+            }
+        })
+    })
+}
+
+loadVideos()
---
+++ videos.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        src: "./img/01.mp4",
+        title: "Aula 01",
+    },
+    {
+        src: "./img/02.mp4",
+        title: "Aula 02",
+    },
+    {
+        src: "./img/03.mp4",
+        title: "Aula 03",
+    },
+    {
+        src: "./img/04.mp4",
+        title: "Aula 04",
+    },
+    {
+        src: "./img/05.mp4",
+        title: "Aula 05",
+    },
+    {
+        src: "./img/06.mp4",
+        title: "Aula 06",
+    },
+    {
+        src: "./img/07.mp4",
+        title: "Aula 07",
+    },
+];
